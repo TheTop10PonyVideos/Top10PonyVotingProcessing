cdbf68c48c3eb02bec8203adc8742dbc92341fea
Vari137 <alvarofernandezgonzalo@gmail.com>
2023-10-15 16:03:12 +0200

    Draft with blacklist diagram pushed for availability (No code)

---
+++ Blacklist.py
@@ -0,0 +1,15 @@
+# NOT WORKING CODE, PUSHED FOR AVAILABILITY BETWEEN DEVICES
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import csv
+
+api_key = "API KEY LOL"  # may replace this lol
+
+csv_file = "Top_10_Pony_Videos.csv"
+"""
+for i in blacklist:
+    for row in videos:
+        if row["channel"] == blacklist:
+            remove from list (ask later if with statement)
+"""
