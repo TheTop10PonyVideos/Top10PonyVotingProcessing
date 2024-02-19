d26c754827f50b16251378606e8b70219681a3d3
Vari137 <alvarofernandezgonzalo@gmail.com>
2024-02-19 22:55:44 +0100

    Calc properly fixed now

--- calculation/calc.py
+++ calculation/calc.py
@@ -18,10 +18,11 @@ def analyze_and_write_titles_to_csv(input_file, output_file=output_titles_path):
     ) as csvfile:  # Opens the CSV and checks number of votes for every title
         csvreader = csv.reader(csvfile)
         for row in csvreader:
-            for title_value in row[1:]:  # Skip column
-                if title_value.strip():
-                    title_counts[title_value] = title_counts.get(title_value, 0) + 1
-                    total_title_count += 1
+            for index, title_value in enumerate(row[3:], start=2):  # Skip column
+                if index % 2 != 0:
+                    if title_value.strip():
+                        title_counts[title_value] = title_counts.get(title_value, 0) + 1
+                        total_title_count += 1
 
             total_title_count -= 1
 
