f560dee605c63d40c661aa1fed29dc5a0cce45d1
StarshinePony <128289655+StarshinePony@users.noreply.github.com>
2024-03-05 23:53:59 +0100

    Fix the private video disaster

--- modules/duration_check.py
+++ modules/duration_check.py
@@ -38,11 +38,11 @@ def check_duration(input):
                             duration,
                             upload_date_str,
                         ) = data_pulling.yt_api(video_id)
-                        if seconds:
+                        if int(duration):
                             seconds = int(duration)
                         else:
                             print("[DARATION CHECK] ERROR NO VIDEO DATA PROCEEDING WITH '0'")
-                            return
+                            continue
 
                         if (
                             seconds <= 30
