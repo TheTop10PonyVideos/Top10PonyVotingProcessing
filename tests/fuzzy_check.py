6c206d95b54fd4579693b8c9a4437073e3b5fe49
hawthornbunny <hawthornrabbit@gmail.com>
2024-03-08 12:46:00 +0000

    Add extra tests for similarity check

--- tests/fuzzy_check.py
+++ tests/fuzzy_check.py
@@ -57,27 +57,89 @@ class TestFuzzyCheck(TestCase):
 
 
     def test_check_similarities(self):
-        rows_a = [
-            ["Cell A1", "Cell A2", "Cell A3"],
-            ["Cell A4", "Cell A5", "Cell A6"],
+        # Test empty CSV
+        rows = []
+        similarities = check_similarities(rows)
+        self.assertEqual(0, len(similarities))
+
+        # Test single value CSV
+        rows = [['Value 1']]
+        similarities = check_similarities(rows)
+        self.assertEqual(0, len(similarities))
+        
+        # Test single column CSV
+        rows = [
+            ['Value 1'],
+            ['Value 2']
         ]
-        similarities = check_similarities(rows_a)
+        similarities = check_similarities(rows)
+        self.assertEqual(0, len(similarities))
+        
+        # Test 2x2 CSV: should produce no results, as the first column isn't
+        # included in similarity checks.
+        rows = [
+            ['Value 1', 'Value 2'],
+            ['Value 3', 'Value 4']
+        ]
+        similarities = check_similarities(rows)
+        self.assertEqual(0, len(similarities))
 
         # We expect 4 similarity results:
         #
-        # * Row 0, index 1: "Cell A2"
-        # * Row 0, index 2: "Cell A3"
-        # * Row 1, index 1: "Cell A5"
-        # * Row 1, index 2: "Cell A6"
+        # * Row 0, column 1: "Value 2"
+        # * Row 0, column 2: "Value 3"
+        # * Row 1, column 1: "Value 5"
+        # * Row 1, column 2: "Value 6"
         #
-        # "Cell A1" and "Cell A4" aren't included as the similarity check always
+        # "Value 1" and "Value 4" aren't included as the similarity check always
         # skips the first column.
-        #
-        # rows_b isn't included as the similarity check doesn't actually check
-        # anything in the second set of rows.
-
+        rows = [
+            ["Value 1", "Value 2", "Value 3"],
+            ["Value 4", "Value 5", "Value 6"],
+        ]
+        similarities = check_similarities(rows)
         self.assertEqual(4, len(similarities))
         self.assertIn((0, 1), similarities)
         self.assertIn((0, 2), similarities)
         self.assertIn((1, 1), similarities)
         self.assertIn((1, 2), similarities)
+        self.assertEqual("Value 2", similarities[(0, 1)][0])
+        self.assertEqual("Value 3", similarities[(0, 2)][0])
+        self.assertEqual("Value 5", similarities[(1, 1)][0])
+        self.assertEqual("Value 6", similarities[(1, 2)][0])
+
+        # Test that empty values are skipped
+        rows = [
+            ["Value 1", "", "Value 2", "", "Value 3", ""],
+            ["Value 4", "", "Value 5", "", "Value 6", ""],
+        ]
+        similarities = check_similarities(rows)
+        self.assertEqual(4, len(similarities))
+        self.assertIn((0, 2), similarities)
+        self.assertIn((0, 4), similarities)
+        self.assertIn((1, 2), similarities)
+        self.assertIn((1, 4), similarities)
+        self.assertEqual("Value 2", similarities[(0, 2)][0])
+        self.assertEqual("Value 3", similarities[(0, 4)][0])
+        self.assertEqual("Value 5", similarities[(1, 2)][0])
+        self.assertEqual("Value 6", similarities[(1, 4)][0])
+
+        # Test non-similarities
+        rows = [
+            ["AAAAAAAA", "", "BBBBBBBB", "", "CCCCCCCC", "", "DDDDDDDD", ""],
+            ["AAAAAAAA", "", "BBBBBBBB", "", "BBBBBBBB", "", "CCCCCCCC", ""],
+            ["AAAAAAAA", "", "BBBBBBBB", "", "CCCCCCCC", "", "BBBBBBBB", ""],
+            ["AAAAAAAA", "", "CCCCCCCC", "", "BBBBBBBB", "", "BBBBBBBB", ""],
+            ["AAAAAAAA", "", "BBBBBBBB", "", "BBBBBBBB", "", "BBBBBBBB", ""],
+        ]
+        similarities = check_similarities(rows)
+        self.assertEqual(9, len(similarities))
+        self.assertIn((1, 2), similarities)
+        self.assertIn((1, 4), similarities)
+        self.assertIn((2, 2), similarities)
+        self.assertIn((2, 6), similarities)
+        self.assertIn((3, 4), similarities)
+        self.assertIn((3, 6), similarities)
+        self.assertIn((4, 2), similarities)
+        self.assertIn((4, 4), similarities)
+        self.assertIn((4, 6), similarities)
